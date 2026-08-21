58e9849114fddcf606121c739c67cf345723af1c
nahguav123 <vaughan@hathaway.nz>
2026-08-21 15:35:21 +1200

    Fixed .env db cred connection

--- server/database/connection.js
+++ server/database/connection.js
@@ -7,12 +7,12 @@ import mariadb from "mariadb";
 // ==========================================
 
 export const dbpool = mariadb.createPool({
-    host: process.env.DB_HOST || "localhost",
-    port: Number(process.env.DB_PORT) || 3306,
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
 
-    user: process.env.DB_USER || "webtycoon",
-    password: "webpass", // process.env.DB_PASSWORD ||
-    database: process.env.DB_NAME || "webtycoon",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
 
     connectionLimit: 10,
 
